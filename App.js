e466956a869692f2d7404130debab38b17a8360b
samueljmillward <samuel.j.pearce@hotmail.com>
2021-05-05 18:54:42 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        To share a photo from your phone with a friend, just press the button
+        below!
+      </Text>
     </View>
   );
 }
